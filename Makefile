062e21311dcffea1041853d1c18feaeef65ec547
Salmireles <sal.chemicale@gmail.com>
2022-03-09 23:01:16 -0800

    another test

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ install:
 	pip install poetry==1.1.12
 	poetry install ${POETRY_ARGS}
 	poetry shell
+	# configure the local git hooks path to run the path in the repo
+	git config core.hooksPath .githooks
--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ def index():
     user = {"username": "Fellow Pyrates"}
     posts = [
         {"author": {"username": "Sal"}, "body": "Beautiful day in Portland!"},
+        
         {"author": {"username": "Michael"}, "body": "Pyrates are so cool!"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
