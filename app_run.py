641767643c843e3ec3826772604692eb6d65a9f4
Salmireles <sal.chemicale@gmail.com>
2022-04-26 23:02:55 -0700

    add shell context imports

--- app_run.py
+++ app_run.py
@@ -1,1 +1,10 @@
-from app import app  # noqa
+from app import app, db  # noqa
+
+from app.models import User, Coin
+
+
+# use `flask shell` to pre-import these in the shell context
+# Usage: `db`, `User`, `Coin`
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Coin": Coin}
