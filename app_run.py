207287670cad4326b450075aa3b22e98b2e1b85e
Salmireles <sal.chemicale@gmail.com>
2022-05-05 23:39:54 -0700

    container fixes

--- app_run.py
+++ app_run.py
@@ -1,5 +1,5 @@
+import click
 from app import app, db  # noqa
-
 from app.models import User, Coin
 
 
@@ -8,3 +8,19 @@ from app.models import User, Coin
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Coin": Coin}
+
+
+# docker usage: docker-compose exec flask-app flask create_db
+@app.cli.command("create_db")
+def create_db():
+    click.echo("Initializing the db")
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@app.cli.command("seed_db")
+def seed_db():
+    click.echo("Seeding the db")
+    db.session.add(User(email="michael@mherman.org"))
+    db.session.commit()
