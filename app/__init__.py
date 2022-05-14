b7a159a32290681bf3b5d0c38b24eeb3b63d1021
Allyson Phillips <61166522+CloudF1sh@users.noreply.github.com>
2022-05-13 20:14:49 -0700

    Updated with registration page
    The registration page is functional, but still struggling to connect with the database.  Once this is resolved, I will be adding the user's personal page, accessible post login.

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 # Construct core Flask application with embedded Dash app.
 app = Flask(__name__, instance_relative_config=False)
 Bootstrap(app)
-
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view='login'
+
+from app import routes,models
 
 
 with app.app_context():
     # Import parts of our core Flask app
-    from app import routes, errors  # noqa
+    from app import routes, errors, models  # noqa
 
     # Import Dash application
     from .plotly_dash.dashboard import init_dashboard
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,57 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from app import app,db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {"username": "Fellow Pyrates"}
     posts = [
         {"author": {"username": "Sal"}, "body": "Beautiful day in Portland!"},
         {"author": {"username": "Michael"}, "body": "Pyrates are so cool!"},
         {"author": {"username": "Scott"}, "body": "Ooh, Bootstrap!"},
+        {"author": {"username": "Allyson"}, "body": "Hello World!"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("wip.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc !='':
+            next_page = url_for('index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("wip.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('WELCOME!  YOU GOT CRYPTO!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
