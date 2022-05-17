db2469c23cffc8a6037f46bd836db90fbe674b4f
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2022-05-16 21:08:05 -0700

    Fingers crossed!

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 # Construct core Flask application with embedded Dash app.
@@ -11,6 +12,10 @@ Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view='login'
+
+from app import routes,models
 
 
 with app.app_context():
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 user_coins = db.Table(
@@ -13,7 +15,7 @@ user_coins = db.Table(
 )
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -26,6 +28,15 @@ class User(db.Model):
     def __repr__(self):
         return "<User {}>".format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Coin(db.Model):
     __tablename__ = "coin"
