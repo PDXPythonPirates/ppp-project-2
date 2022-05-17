61c7bed6dfba23a36ef80487216079b0c2a0b373
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2022-05-17 16:24:32 -0700

    >_<

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 user_coins = db.Table(
@@ -33,6 +34,10 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicons&s={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
