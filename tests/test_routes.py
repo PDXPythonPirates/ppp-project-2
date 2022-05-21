f38961eea757a3ebb7f0c4b26b2be07fdb03639a
Scott-Larsen <scott@scottlarsen.com>
2022-05-21 12:34:00 -0700

    Add tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,12 +6,27 @@ from app import app
 class TestRoutes:
     """Test all routes."""
 
-    def test_index_route(self):
+    def test_blank_route(self):
         response = app.test_client().get("/")
+
+        assert response.status_code == 200
+
+    def test_index_route(self):
+        response = app.test_client().get("/index")
 
         assert response.status_code == 200
 
     def test_login_route(self):
         response = app.test_client().get("/login")
+
+        assert response.status_code == 200
+
+    # def test_logout_route(self):
+    #     response = app.test_client().get("/logout")
+
+    #     assert response.status_code == 302
+
+    def test_register_route(self):
+        response = app.test_client().get("/register")
 
         assert response.status_code == 200
