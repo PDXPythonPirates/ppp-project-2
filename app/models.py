bb8c4beb8edccd19d938e1070d85c382a9ee6429
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2022-05-24 15:53:09 -0700

    removed a letter XD

--- app/models.py
+++ app/models.py
@@ -35,7 +35,7 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-@login.user_loaders
+@login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 class Coin(db.Model):
